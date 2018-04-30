bffa402a527c9acfeda44abb036d0be22b2654b2
israel <imoralescs@gmail.com>
2018-04-30 17:28:53 -0400

    comment

--- schema/dump.sql
+++ schema/dump.sql
@@ -1,6 +1,6 @@
 CREATE TABLE users 
 (
-  id INT(11) UNSIGNED NOT NULL AUTO_INCREMENT PRIMARY KEY, 
+  id INT(11) NOT NULL AUTO_INCREMENT PRIMARY KEY, 
   name VARCHAR(255) NOT NULL, 
   email VARCHAR(255) NOT NULL,
   password VARCHAR(255) NOT NULL,
@@ -8,25 +8,42 @@ CREATE TABLE users
   remember_identifier VARCHAR(255)
 );
 
-INSERT INTO users (id, name, email, password, remember_token, remember_identifier)
-VALUES (1, "Israel Morales", "imoralescs@gmail.com", "$2a$12$jWz5man/w84aqb5Tu7I7JuP8.CSo8VNN/g7spotrfD7EGIgJkA0G.", null, null);
-INSERT INTO users (id, name, email, password, remember_token, remember_identifier)
-VALUES (2, "Javier Perez", "perecito@gmail.com", "$2a$12$jWz5man/w84aqb5Tu7I7JuP8.CSo8VNN/g7spotrfD7EGIgJkA0G.", null, null);
-INSERT INTO users (id, name, email, password, remember_token, remember_identifier)
-VALUES (3, "Michelle Louis", "michellin@gmail.com", "$2a$12$jWz5man/w84aqb5Tu7I7JuP8.CSo8VNN/g7spotrfD7EGIgJkA0G.", null, null);
-INSERT INTO users (id, name, email, password, remember_token, remember_identifier)
-VALUES (4, "Stephany Mcnael", "stephkitty@gmail.com", "$2a$12$jWz5man/w84aqb5Tu7I7JuP8.CSo8VNN/g7spotrfD7EGIgJkA0G.", null, null);
-INSERT INTO users (id, name, email, password, remember_token, remember_identifier)
-VALUES (5, "Pedro Diaz", "pedrito@gmail.com", "$2a$12$jWz5man/w84aqb5Tu7I7JuP8.CSo8VNN/g7spotrfD7EGIgJkA0G.", null, null);
-INSERT INTO users (id, name, email, password, remember_token, remember_identifier)
-VALUES (6, "Adam Warlock", "adamhatethano@gmail.com", "$2a$12$jWz5man/w84aqb5Tu7I7JuP8.CSo8VNN/g7spotrfD7EGIgJkA0G.", null, null);
+INSERT INTO 
+  users (id, name, email, password, remember_token, remember_identifier)
+VALUES 
+  (1, "Israel Morales", "imoralescs@gmail.com", "$2a$12$jWz5man/w84aqb5Tu7I7JuP8.CSo8VNN/g7spotrfD7EGIgJkA0G.", null, null),
+  (2, "Javier Perez", "perecito@gmail.com", "$2a$12$jWz5man/w84aqb5Tu7I7JuP8.CSo8VNN/g7spotrfD7EGIgJkA0G.", null, null),
+  (3, "Michelle Louis", "michellin@gmail.com", "$2a$12$jWz5man/w84aqb5Tu7I7JuP8.CSo8VNN/g7spotrfD7EGIgJkA0G.", null, null),
+  (4, "Stephany Mcnael", "stephkitty@gmail.com", "$2a$12$jWz5man/w84aqb5Tu7I7JuP8.CSo8VNN/g7spotrfD7EGIgJkA0G.", null, null),
+  (5, "Pedro Diaz", "pedrito@gmail.com", "$2a$12$jWz5man/w84aqb5Tu7I7JuP8.CSo8VNN/g7spotrfD7EGIgJkA0G.", null, null),
+  (6, "Adam Warlock", "adamhatethano@gmail.com", "$2a$12$jWz5man/w84aqb5Tu7I7JuP8.CSo8VNN/g7spotrfD7EGIgJkA0G.", null, null);
 
 CREATE TABLE friendships
 (
-  id INT(11) UNSIGNED NOT NULL AUTO_INCREMENT PRIMARY KEY, 
-  name VARCHAR(255) NOT NULL, 
-  email VARCHAR(255) NOT NULL,
-  password VARCHAR(255) NOT NULL,
-  remember_token VARCHAR(255),
-  remember_identifier VARCHAR(255)
+  friend_id INT(11) NULL,
+  user_id INT(11) NULL,
+  FOREIGN KEY(user_id) 
+  REFERENCES users(id)
 );
+
+INSERT INTO 
+  friendships (user_id, friend_id)
+VALUES 
+  (1, 2), 
+  (2, 1), 
+  (3, 4), 
+  (4, 3), 
+  (1, 5),
+  (5, 1),
+  (1, 6),
+  (6, 1),
+  (2, 5),
+  (5, 2),
+  (2, 6),
+  (6, 2),
+  (5, 6),
+  (6, 5);
+
+/* Query Mysql: */
+/* Friends */
+/* SELECT * from friendships JOIN users ON users.id = friendships.friend_id WHERE friendships.user_id = 2; */
--- src/routes/api.php
+++ src/routes/api.php
@@ -9,66 +9,41 @@ use GraphQL\Schema;
 use GraphQL\Type\Definition\Type;
 use GraphQL\Type\Definition\ObjectType;
 
-// Used to run JavaScript
-use Nacmartin\PhpExecJs\PhpExecJs;
+$route->get('/graphql', function(ServerRequestInterface $request, ResponseInterface $response, array $args){
+    try 
+    {
+        $pdo = new PDO('mysql:host=mysql;dbname=name_db', 'user', 'secret');
+        $res = 'Connected to MySQL';
+    
+    }
+    catch (PDOException $e) 
+    {
+        var_dump('Error: ' . $e->getMessage());
+        exit();
+    }
 
-$route->get('/api', function(ServerRequestInterface $request, ResponseInterface $response){
-    // Retrieve data from $request. Do what you need to do and build your $content.
-
-    // Render JavaScript array
-    // $phpexecjs = new PhpExecJs();
-    /* $content = $phpexecjs->evalJs("
-        'red yellow blue'.split(' ')
-    ");
-    */
-    // $response->getBody()->write(json_encode($content));
-
-    // Render JavaScript function
-    $phpexecjs = new PhpExecJs();
-    $content = $phpexecjs->evalJs("
-        var worlds = 'Worlds';
-        const hello = 'Hello';
-        function greeting(str) {
-            return hello + ', ' + str + '!';
-        }
-
-        greeting(worlds);
-    ");
-
-    $response->getBody()->write(json_encode($content));
-
-    return $response->withStatus(200);
-});
-
-$route->get('/api/{id}', function(ServerRequestInterface $request, ResponseInterface $response, array $args){
-    $itemId = $args['id'];
-    $requestBody = json_decode($request->getBody(), true);
-
-    // Possibly update a record.
-
-    $response->getBody()->write(json_encode($itemId));
+    $response->getBody()->write($res);
     return $response->withStatus(202);
 });
-
 /*
-$route->get('/api/400', function(ServerRequestInterface $request, ResponseInterface $response){
-    // If we fail to bad request.
-    throw new BadRequestException;
-});
-*/
-
 $route->post('/graphql', function(ServerRequestInterface $request, ResponseInterface $response, array $args){
-    // Note: To know $request and $response method used, Zend Diactoros ServerRequest Documentation.
-    // $request->getQueryParams();
+    /*
+     * Note: To know $request and $response method used, Zend Diactoros ServerRequest Documentation.
+     * $request->getQueryParams();
+     **/
 
     /** 
      * Query GraphQL
      * Postman:
      *   Verb - POST
-     *   Body Raw - {"query": "query { echo(message: \"Hi Worlds, GraphQL!\") }" }
-     *   Response - {"data":{"echo":"You said: Hi Worlds, GraphQL!"}}
+     *   Query:
+     *     Body raw - {"query": "query { echo(message: \"Hi Worlds, GraphQL!\") }"}
+     *     Response - {"data":{"echo":"You said: Hi Worlds, GraphQL!"}}
+     *   Mutation:
+     *     Body raw - {"query": "mutation { sum(x: 2, y: 2) }"}
+     *     Response - {"data":{"sum":4}}
      */
-
+/*
     try {
         
         $queryType = new ObjectType([
@@ -88,8 +63,25 @@ $route->post('/graphql', function(ServerRequestInterface $request, ResponseInter
             ]
         ]);
 
+        $mutationType = new ObjectType([
+            'name' => 'Calc',
+            'fields' => [
+                'sum' => [
+                    'type' => Type::int(),
+                    'args' => [
+                        'x' => ['type' => Type::int()],
+                        'y' => ['type' => Type::int()],
+                    ],
+                    'resolve' => function ($root, $args) {
+                        return $args['x'] + $args['y'];
+                    },
+                ],
+            ],
+        ]);
+
         $schema = new Schema([
-            'query' => $queryType
+            'query' => $queryType,
+            'mutation' => $mutationType
         ]);
 
         $rawInput = file_get_contents('php://input');
@@ -99,6 +91,170 @@ $route->post('/graphql', function(ServerRequestInterface $request, ResponseInter
 
         $rootValue = ['prefix' => 'You said: '];
         $result = GraphQL::executeQuery($schema, $query, $rootValue, null, $variableValues);
+        $output = $result->toArray();
+    }
+    catch(\Exception $error) {
+        $output = [
+            'error' => [
+                'message' => $error->getMessage()
+            ]
+        ];
+    }
+    $response->getBody()->write(json_encode($output));
+    return $response->withStatus(202);
+});
+*/
+
+class DB
+{
+    private static $pdo;
+    public static function init()
+    {
+        $user = 'user';
+        $pass = 'password';
+        $dsn = "mysql:host=mysql;port=3306;dbname=name_db;";
+        // Создаем PDO соединение
+        self::$pdo = new PDO($dsn, $user, $pass);
+        // Задаем режим выборки по умолчанию
+        self::$pdo->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_OBJ);
+    }
+
+    public static function selectOne($query)
+    {
+        $records = self::select($query);
+        return array_shift($records);
+    }
+
+    public static function select($query)
+    {
+        $statement = self::$pdo->query($query);
+        return $statement->fetchAll();
+    }
+
+    public static function affectingStatement($query)
+    {
+        $statement = self::$pdo->query($query);
+        return $statement->rowCount();
+    }
+}
+
+class UserType extends ObjectType
+{
+    public function __construct()
+    {
+        $config = [
+            'description' => 'Пользователь',
+            'fields' => function() {
+                return [
+                    'id' => [
+                        'type' => Types::string(),
+                        'description' => 'Идентификатор пользователя'
+                    ],
+                    'name' => [
+                        'type' => Types::string(),
+                        'description' => 'Имя пользователя'
+                    ],
+                    'email' => [
+                        'type' => Types::string(),
+                        'description' => 'E-mail пользователя'
+                    ],
+                    'friends' => [
+                        'type' => Types::listOf(Types::user()),
+                        'description' => 'Друзья пользователя',
+                        'resolve' => function ($root) {
+                            return DB::select("SELECT u.* from friendships f JOIN users u ON u.id = f.friend_id WHERE f.user_id = {$root->id}");
+                        }
+                    ],
+                    'countFriends' => [
+                        'type' => Types::int(),
+                        'description' => 'Количество друзей пользователя',
+                        'resolve' => function ($root) {
+                            return DB::affectingStatement("SELECT u.* from friendships f JOIN users u ON u.id = f.friend_id WHERE f.user_id = {$root->id}");
+                        }
+                    ]
+                ];
+            }
+        ];
+        parent::__construct($config);
+    }
+}
+
+class QueryType extends ObjectType
+{
+    public function __construct()
+    {
+        $config = [
+            'fields' => function() {
+                return [
+                    'user' => [
+                        'type' => Types::user(),
+                        'description' => 'Возвращает пользователя по id',
+                        'args' => [
+                            'id' => Types::int()
+                        ],
+                        'resolve' => function ($root, $args) {
+                            return DB::selectOne("SELECT * from users WHERE id = {$args['id']}");
+                        }
+                    ],
+                    'allUsers' => [
+                        'type' => Types::listOf(Types::user()),
+                        'description' => 'Список пользователей',
+                        'resolve' => function () {
+                            return DB::select('SELECT * from users');
+                        }
+                    ]
+                ];
+            }
+        ];
+        parent::__construct($config);
+    }
+}
+
+class Types
+{
+    private static $query;
+    private static $user;
+
+    public static function query()
+    {
+        return self::$query ?: (self::$query = new QueryType());
+    }
+
+    public static function user()
+    {
+        return self::$user ?: (self::$user = new UserType());
+    }
+
+    public static function int()
+    {
+        return Type::int();
+    }
+
+    public static function string()
+    {
+        return Type::string();
+    }
+
+    public static function listOf($type)
+    {
+        return Type::listOf($type);
+    }
+}
+
+$route->post('/graphql', function(ServerRequestInterface $request, ResponseInterface $response, array $args){
+    try {
+        
+        DB::init();
+
+        $schema = new Schema([
+            'query' => Types::query()
+        ]);
+
+        $rawInput = file_get_contents('php://input');
+        $input = json_decode($rawInput, true);
+        $query = $input['query'];
+
+        $result = GraphQL::executeQuery($schema, $query);
         $output = $result->toArray();
     }
     catch(\Exception $error) {
